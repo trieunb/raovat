6fc67df435ca790efa5aecc1e8dcce1de6d70dac
trieunb <trieunb08@gmail.com>
2015-06-23 17:00:05 +0700

    ac

--- app/DTT/Forms/TuyenDungForm.php
+++ app/DTT/Forms/TuyenDungForm.php
@@ -4,10 +4,23 @@ class TuyenDungForm extends FormValidator {
 
 	protected $rules = array(
 		'tencty'	=>	'required|min:4|max:255',
+		'diachi'	=>	'required',
+		'linhvuc'	=> 	'required',
+		'vitrituyendung'	=>	'required',
+		'nguoidangtin'	=>	'required',
+		'chucvu'	=>	'required',
+		'sodienthoai'	=>	'required'
+
 		
 		);
 	protected $messages = array(
 		'tencty.required'	=>	'Tiêu đề không được trống.',
-		
+		'diachi.required'	=>	'Địa chỉ không được trống.',
+		'linhvuc.required'	=>	'Lĩnh vực không được trống.',
+		'vitrituyendung.required'	=>	'Vị trí tuyển dụng không được trống.',
+		'nguoidangtin.required'	=>	'Người đăng tin không được trống.',
+		'chucvu.required'	=>	'Chức vụ không được trống.',
+		'sodienthoai.required'	=>	'Số điện thoại không được trống.',
+
 		);
 }
